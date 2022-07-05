41c634b8023d09423136b07910cd5b061103e865
albo <lxmrtn7@gmail.com>
2022-07-05 11:57:08 +0100

    wip information page styling
    (cherry picked from commit 8d661f0404d70b31be8deef90aad01b26199b28a)

--- src/views/Page/InformationPage.tsx
+++ src/views/Page/InformationPage.tsx
@@ -19,7 +19,7 @@ function InformationPage(props: any) {
   const getImg = (pageId: string) => {
     return `${apiBase}/pages/${pageId}/image.png?max_dimension=900`;
   };
-
+  console.log(props.content)
   return (
     <div className="information-page">
       <Helmet>
@@ -38,63 +38,101 @@ function InformationPage(props: any) {
       />
       <section className="information-page__overview">
         <div className="flex-container">
-          {props.content.title && (
-            <div className="flex-col flex-col--12">
-              <h1 className="information-page__heading">{props.content.title}</h1>
+          <div className="cms--contact-card">
+            <div className="flex-container">
+              <div className="flex-col flex-col--8 landing-page__intro">
+                {props.content.title && (
+                   <h1 className="information-page__heading">{props.content.title}</h1>
+                 
+                )}
+                {props.content.excerpt && (
+                  <ReactMarkdown
+                    children={props.content.excerpt}
+                    className="information-page__content"
+                  />
+                )}
+              </div>
+
+              <div className="flex-col flex-col--4 landing-page__image">
+                <div className='parent-page-image'>
+                  {props.content.parent.image && (
+                    <img
+                      alt={props.content.parent.title ? props.content.parent.title : ''}
+                      className="image"
+                      src={getImg(props.content.parent.id)}
+                    />
+                  )}
+                  {props.content.parent.title && (
+                    <div className='title'>{props.content.parent.title}</div>
+                  )}
+                </div>
+              </div>
             </div>
-          )}
-          {props.content.image && (
-            <div className="flex-col flex-col--12 information-page__image">
-              <img alt={props.content.title} className="image" src={getImg(props.content.id)} />
-            </div>
-          )}
-          {props.content.content.introduction.copy && (
-            <div className="flex-col flex-col--12">
-              <ReactMarkdown
-                children={props.content.content.introduction.copy[0]}
-                className="information-page__content markdown"
+          </div>
+        </div>
+
+        <div className="flex-container">
+          <div className="flex-col flex-col--8">
+            {props.content.image && (
+              <img
+                alt={props.content.title ? props.content.title : ''}
+                className="article-image"
+                src={getImg(props.content.id)}
               />
-            </div>
-          )}
-          <div className="flex-col flex-col--12 information-page__more">
-            {pageIllo && (
-              <div className="flex-col">
-                <img
-                  alt="Mum and child walking together"
-                  className="information-page__illustration"
-                  src={pageIllo}
+            )}
+            {props.content.content.introduction.copy && (
+              <div>
+                <ReactMarkdown
+                  children={props.content.content.introduction.copy[0]}
+                  className="information-page__content markdown"
                 />
               </div>
             )}
+
+            {props.content.children.filter((child: IPage) => child.enabled).length > 0 && (
+              <div>
+                <h2 className="information-page__sub-heading">In this topic</h2>
+                <div className="information-page__pages button__excerpt--peach">
+                  {props.content.children
+                    .filter((child: IPage) => child.enabled)
+                    .sort((a: { order: number }, b: { order: number }) => a.order - b.order)
+                    .map((page: { id: string; title: string; icon: IconName; excerpt: string }) => {
+                      return (
+                        <ButtonLink
+                          href={'/' + page.id}
+                          text={page.title}
+                          key={page.id}
+                          category={true}
+                          icon={page.icon}
+                          excerpt={{
+                            isExcerpt: true,
+                            text: page.excerpt
+                          }}
+                        />
+                      );
+                    })}
+                </div>
+              </div>
+            )}
           </div>
+          <div className="flex-col flex-col--4">
+            <div className="information-page__sitemap">
+              sitemap goes here
+            </div>  
+          </div>  
+        </div>
+        <div className="information-page__more">
+          {pageIllo && (
+            <div className="flex-col">
+              <img
+                alt="Mum and child walking together"
+                className="information-page__illustration"
+                src={pageIllo}
+              />
+            </div>
+          )}
         </div>
       </section>
-
-      {props.content.children.filter((child: IPage) => child.enabled).length > 0 && (
-        <section className="information-page__other">
-          <div className="flex-container">
-            <div className="flex-col flex-col--12">
-              <h2 className="information-page__sub-heading">Other pages in this section</h2>
-            </div>
-            <div className="flex-col flex-col--12 information-page__pages">
-              {props.content.children
-                .filter((child: IPage) => child.enabled)
-                .sort((a: { order: number }, b: { order: number }) => a.order - b.order)
-                .map((page: { id: string; title: string; icon: IconName }) => {
-                  return (
-                    <ButtonLink
-                      href={'/' + page.id}
-                      text={page.title}
-                      key={page.id}
-                      category={true}
-                      icon={page.icon}
-                    />
-                  );
-                })}
-            </div>
-          </div>
-        </section>
-      )}
     </div>
   );
 }
